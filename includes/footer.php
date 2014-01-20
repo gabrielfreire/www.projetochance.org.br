d243ee65be5e055ab287e60fa27377f44c43cd44
Gabriel Freire <gabrielfreire14@hotmail.com>
2014-01-20 01:27:30 -0200

    Definindo novo layout

---
+++ includes/footer.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * Rodapé do site
+ */
+
+?>
+
+<footer>
+  <p>Projeto Chance - Todos os direitos reservados ® - Rua Melchior Giola, 646 - Paraisópolis - São Paulo - SP / 11 2866-3441</p>
+</footer>
+
+<!-- javascript at the bottom for fast page loading -->
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/jquery.easing-sooper.js"></script>
+<script type="text/javascript" src="js/jquery.sooperfish.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('ul.sf-menu').sooperfish();
+    });
+</script>
---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<?php
+
+/* 
+ * Cabeçalho do site
+ */
+
+?>
+
+<header>
+    <label id="saudacao">Sábado, 20 de setembro de 2014</label>
+    
+    <a href="index.html">
+        <img src="./images/logo-original.png" alt="Logotipo" title="Projeto Chance" id="logo" />
+    </a>
+    
+    <nav>
+        <ul class="sf-menu" id="nav">
+            <li><a href="#">O Projeto Chance</a></li>
+            <li><a href="#">Matrícula</a></li>
+            <li><a href="#">Proposta pedagógica</a></li>
+            <li><a href="#">Aprovados</a>
+                <ul>
+                  <li><a href="#">Drop Down 1</a></li>
+                  <li><a href="#">Drop Down 2</a></li>
+                  <li><a href="#">Drop Down 3</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Galeria de fotos</a></li>
+            <li><a href="#">Contato</a></li>
+        </ul>
+    </nav>
+</header>
