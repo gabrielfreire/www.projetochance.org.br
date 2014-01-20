812fe718a2a48982a25f599a20429a63f7dc59c1
Gabriel Freire <gabrielfreire14@hotmail.com>
2014-01-20 17:15:23 -0200

    animação na caixa de busca

--- includes/footer.php
+++ includes/footer.php
@@ -11,11 +11,27 @@
 </footer>
 
 <!-- javascript at the bottom for fast page loading -->
-<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
 <script type="text/javascript" src="js/jquery.easing-sooper.js"></script>
 <script type="text/javascript" src="js/jquery.sooperfish.js"></script>
 <script type="text/javascript">
     $(document).ready(function() {
+        
+        /* Ativar efeito no menu drop down */
         $('ul.sf-menu').sooperfish();
+        
+        /* Ativar efeito na caixa de busca */
+        $("input:text", "#box-buscar").on({
+            focus: function (){
+                $(this).animate({
+                    width: "100px"
+                }, 300);
+            },
+            focusout: function (){
+                $(this).animate({
+                    width: "60px"
+                }, 300);
+            }
+        });
     });
 </script>
