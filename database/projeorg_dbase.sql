d76fd7cf84af978e3f96955f200ec9a8cf31209b
Gabriel Freire <gabrielfreire14@hotmail.com>
2014-01-26 13:52:33 -0200

    add tabela de contato

--- database/projeorg_dbase.sql
+++ database/projeorg_dbase.sql
@@ -20,8 +20,18 @@ CREATE TABLE IF NOT EXISTS aluno (
     telefone varchar(15) not null,
     email varchar(200) not null,
     senha varchar(50) not null,
-    ano_conclusao_em varchar(4) not null,
-    ano_prova_enem varchar(4) not null,
-    nome_inst varchar(200) not null,
-    nome_cursinho varchar(200) not null
+    ano_conclusao_em varchar(4) default null,
+    ano_prova_enem varchar(4) default null,
+    nome_inst varchar(200) default null,
+    nome_cursinho varchar(200) default null
+) DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS contato (
+    id int primary key auto_increment not null,
+    nome varchar(200) not null,
+    telefone varchar(15) default null,
+    email varchar(200) not null,
+    assunto varchar(200) not null,
+    mensagem text not null
 ) DEFAULT CHARSET=utf8;
