157df9495c407cdb4a222fed50e9a6a39dfc2186
Gabriel Freire <gabrielfreire14@hotmail.com>
2014-01-26 22:03:04 -0200

    add downloads das páginas enem e vestibulares

--- vestibulares.php
+++ vestibulares.php
@@ -15,8 +15,44 @@
             <div id="content">                              
                 <?php include "./includes/left.php"; ?>                
                 
-                <div id="sub-content">
+                <div id="sub-content">                    
+                    <h1>Vestibulares</h1>
                     
+                    <div class="box-link">
+                        <h4>FUVEST</h4>
+                        <a class="prova" href="downloads/vestibulares/5 Fase Prova de conheciementos Gerais- Fuvest 2010.rar">Prova Conhecimentos Gerais</a><br />
+                        <a class="prova" href="downloads/vestibulares/5 Fase - Gabarito Fuvest 2011 Conhecimentos Gerais.rar">Gabarito Conhecimentos Gerais</a><br /><br />
+                    </div>
+                    
+                    <div class="box-link">
+                        <h4>Relação Candidato por vaga FUVEST</h4>
+                        <a class="prova" href="downloads/vestibulares/2011-fuvest-relacao-candidato-vaga-2011.rar">Relação candidato por vaga 2011 FUVEST</a><br /><br />
+                    </div>
+                    
+                    <div class="box-link">
+                        <h4>Unicamp</h4>
+                        <a class="prova" href="downloads/vestibulares/3prova_unicamp_QZ_2011_f1.rar">Vestibular 2011</a><br />
+                        <a class="prova" href="downloads/vestibulares/3Unicamp 2011Gabarito conhecimentos gerais.rar">Gabarito Unicamp 2011</a><br /><br />
+                    </div>
+                    
+                    <div class="box-link">
+                        <h4>Unesp</h4>
+                        <a class="prova" href="downloads/vestibulares/4Unesp - prova.rar">Vestibular 2011</a><br />
+                        <a class="prova" href="downloads/vestibulares/4UNESP - GABARITO DE CONHECIMENTOS GERAIS.rar">Gabarito Unesp 2011</a><br /><br />
+                    </div>
+                    
+                    <div class="box-link">
+                        <h4>Mackenzie</h4>
+                        <a class="prova" href="downloads/vestibulares/1prova_mack_gruposII_III_2010_sem1.rar">Vestibular 2011</a><br />
+                        <a class="prova" href="downloads/vestibulares/1gabarito_mack_gruposII_III_2010_sem1.rar">Gabarito Mackenzie 2011</a><br /><br />
+                    </div>
+                    
+                    <div class="box-link">
+                        <h4>Puc - SP</h4>
+                        <a class="prova" href="downloads/vestibulares/2prova_pucsp_objetiva_2011_sem1.rar">Vestibular 2011</a><br />
+                        <a class="prova" href="downloads/vestibulares/2gabarito_pucsp_objetiva_2011_sem1.rar">Gabarito Puc 2011</a><br />
+                        <a class="prova" href="downloads/vestibulares/prova_pucsp_discursiva_2011_sem1.rar">Prova PUC - SP dissertativa</a><br /><br />
+                    </div>
                 </div><!--sub-content-->        
             </div><!--content-->
 
