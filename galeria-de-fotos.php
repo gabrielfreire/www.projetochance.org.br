2e243506dbb99e27078b1b65e94ae993a7804307
Gabriel Freire <gabrielfreire14@hotmail.com>
2014-01-28 00:01:44 -0200

    add fancybox na galeria e editei biblioteca do js para nativo

--- galeria-de-fotos.php
+++ galeria-de-fotos.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width">
         
         <link rel="stylesheet" type="text/css" href="css/style.css" /> 
+        <link rel="stylesheet" type="text/css" href="js/fancy/jquery.fancybox.css?v=2.1.0" media="screen"/>
     </head>
     <body>
         <div id="main">    
@@ -16,12 +17,79 @@
                 <?php include "./includes/left.php"; ?>                
                 
                 <div id="sub-content">
+                    <h1>Fotos do projeto</h1>
+                    
+                    <div class="thumbnails">                    
+                        <a class="rel thumbnail" data-fancybox-group="gallery" href="images/fotos/img1.jpg">
+                            <img data-src="holder.js/300x200" src="images/fotos/img1.jpg" alt="">
+                        </a>
+                        <a class="rel thumbnail" data-fancybox-group="gallery" href="images/fotos/img2.jpg">
+                            <img data-src="holder.js/300x200" src="images/fotos/img2.jpg" alt="">
+                        </a>                   
+                        <a class="rel thumbnail" data-fancybox-group="gallery" href="images/fotos/img3.jpg">
+                            <img data-src="holder.js/300x200" src="images/fotos/img3.jpg" alt="">
+                        </a>
+                        <a class="rel thumbnail" data-fancybox-group="gallery" href="images/fotos/img4.jpg">
+                            <img data-src="holder.js/300x200" src="images/fotos/img4.jpg" alt="">
+                        </a>
+                    </div>
+                    
+                    <div class="thumbnails"> 
+                        <a class="rel thumbnail" data-fancybox-group="gallery" href="images/fotos/img5.jpg">
+                            <img data-src="holder.js/300x200" src="images/fotos/img5.jpg" alt="">
+                        </a>                   
+                        <a class="rel thumbnail" data-fancybox-group="gallery" href="images/fotos/img6.jpg">
+                            <img data-src="holder.js/300x200" src="images/fotos/img6.jpg" alt="">
+                        </a>
+                        <a class="rel thumbnail" data-fancybox-group="gallery" href="images/fotos/img7.jpg">
+                            <img data-src="holder.js/300x200" src="images/fotos/img7.jpg" alt="">
+                        </a>
+                        <a class="rel thumbnail" data-fancybox-group="gallery" href="images/fotos/img8.jpg">
+                            <img data-src="holder.js/300x200" src="images/fotos/img8.jpg" alt="">
+                        </a>
+                    </div>
+                    
+                    <div class="thumbnails">
+                        <a class="rel thumbnail" data-fancybox-group="gallery" href="images/fotos/img9.jpg">
+                            <img data-src="holder.js/300x200" src="images/fotos/img9.jpg" alt="">
+                        </a>                   
+                        <a class="rel thumbnail" data-fancybox-group="gallery" href="images/fotos/img10.jpg">
+                            <img data-src="holder.js/300x200" src="images/fotos/img10.jpg" alt="">
+                        </a>
+                        <a class="rel thumbnail" data-fancybox-group="gallery" href="images/fotos/img11.jpg">
+                            <img data-src="holder.js/300x200" src="images/fotos/img11.jpg" alt="">
+                        </a>
+                        <a class="rel thumbnail" data-fancybox-group="gallery" href="images/fotos/img12.jpg">
+                            <img data-src="holder.js/300x200" src="images/fotos/img12.jpg" alt="">
+                        </a>
+                    </div>
                     
                 </div><!--sub-content-->        
             </div><!--content-->
 
 
             <?php include "./includes/footer.php"; ?>            
-        </div>
+        </div>        
+        
+        <script src="js/fancy/jquery.fancybox.js?v=2.1.0" type="text/javascript"></script>	
+        <script src="js/fancy/jquery.mousewheel-3.0.6.pack.js" type="text/javascript"></script>	
+        <script type="text/javascript">
+            
+            /*
+             * Ativar fancybox
+             */
+            $("a.rel", "div.thumbnails").fancybox({
+                openEffect : 'elastic',
+                openSpeed  : 150,
+                closeEffect : 'elastic',
+                closeSpeed  : 350, 
+                arrows: true,
+                helpers : {
+                    title : {
+                        type : 'float'//float, over, outside,inside
+                    }
+                }                
+            });
+        </script>
     </body>
 </html>
--- includes/footer.php
+++ includes/footer.php
@@ -12,10 +12,11 @@
 
 
 <!-- javascript padrão (cabeçalho) -->
-<script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="js/jquery.min.js" type="text/javascript"></script>	
 <script type="text/javascript" src="js/jquery.easing-sooper.js"></script>
 <script type="text/javascript" src="js/jquery.sooperfish.js"></script>
 <script type="text/javascript">
+    
     $(document).ready(function() {
         
         /* Ativar efeito no menu drop down */
