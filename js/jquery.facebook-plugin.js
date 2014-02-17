91368f3d378865ee0a54078e9f8c4d00e8b03e5d
Gabriel Freire <gabrielfreire14@hotmail.com>
2014-02-17 02:51:05 -0300

    add plugin do facebook

---
+++ js/jquery.facebook-plugin.js
@@ -0,0 +1,14 @@
+/* 
+ * Plugin do facebook
+ * Usado para fazer comentários e compartilhar artigos.
+ */
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
