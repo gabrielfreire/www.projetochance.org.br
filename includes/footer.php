064c0b9fdc65d88bcc92a16ee6ea4f92efc1e77f
Gabriel Freire <gabrielfreire14@hotmail.com>
2014-02-18 13:26:40 -0300

    correção de css para ie8+

--- includes/footer.php
+++ includes/footer.php
@@ -1,58 +1,56 @@
 <?php
-
-/* 
+/*
  * Rodapé do site
  */
-
 ?>
 
-<footer>
-  <p>Projeto Chance - Todos os direitos reservados ® - Rua Melchior Giola, 646 - Paraisópolis - São Paulo - SP / 11 2866-3441</p>
-</footer>
+<div class="footer">
+    <p>Projeto Chance - Todos os direitos reservados ® - Rua Melchior Giola, 646 - Paraisópolis - São Paulo - SP / 11 2866-3441</p>
+</div>
 
 
-<!-- javascript padrão (cabeçalho) -->
-<script type="text/javascript" src="js/jquery.min.js"></script>	
-<script type="text/javascript" src="js/jquery.home.js"></script>	
+<!-- javascript para todo o site -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.home.js"></script>
 <script type="text/javascript" src="js/jquery.easing-sooper.js"></script>
 <script type="text/javascript" src="js/jquery.sooperfish.js"></script>
 <script type="text/javascript">
-    
+
     $(document).ready(function() {
-        
+
         /* Ativar efeito no menu drop down */
         $('ul.sf-menu').sooperfish();
-        
+
         /* Ativar efeito na caixa de busca */
         $("input:text", "#box-buscar").on({
-            focus: function (){
+            focus: function() {
                 $(this).animate({
                     width: "70px"
                 }, 250);
             },
-            focusout: function (){
+            focusout: function() {
                 $(this).animate({
                     width: "50px"
                 }, 200);
             }
         });
-        
+
         /* Ativar efeito no menu vertical */
         $("li > a", "ol#nav-left").on({
-            mouseover: function (){
+            mouseover: function() {
                 $(this).animate({
                     marginLeft: "10px"
                 }, 85);
             },
-            mouseout: function (){
+            mouseout: function() {
                 $(this).animate({
                     marginLeft: 0
                 }, 85);
             }
         });
     });
-    
-    
+
+
     /*
      * Iniciar login
      */
--- includes/header.php
+++ includes/header.php
@@ -1,51 +1,50 @@
 <?php
-
-/* 
+/*
  * Cabeçalho do site
  */
 
 require "./classes/Data.class.php";
 ?>
 
-<header>
+<div class="header">
     <label id="saudacao">
-        <?php echo ucfirst( Data::getDiaSemana() ).", ".Data::getDia()." de ".Data::getMes()." de ".Data::getAno() ?>
+        <?php echo ucfirst(Data::getDiaSemana()) . ", " . Data::getDia() . " de " . Data::getMes() . " de " . Data::getAno() ?>
     </label>
-    
+
     <a href="index.html">
         <img src="./images/logo-original.png" alt="Logotipo" title="Projeto Chance" id="logo" />
     </a>
-    
-    <nav>
+
+    <div class="nav">
         <ul class="sf-menu" id="nav">
             <li><a href="#">O Projeto Chance</a>
                 <ul>
-                  <li><a href="palavra-do-presidente.html">Palavra do presidente</a></li>
-                  <li><a href="quem-somos.html">Quem somos</a></li>
-                  <li><a href="nossa-historia.html">Nossa história</a></li>
+                    <li><a href="palavra-do-presidente.html">Palavra do presidente</a></li>
+                    <li><a href="quem-somos.html">Quem somos</a></li>
+                    <li><a href="nossa-historia.html">Nossa história</a></li>
                 </ul>
             </li>
             <li><a href="proposta-pedagogica.html">Proposta pedagógica</a></li>
             <li><a href="#">Vestibulares</a>
                 <ul>
-                  <li><a href="historia-do-vestibular.html">História do vestibular</a></li>
-                  <li><a href="principais-vestibulares.html">Principais vestibulares</a></li>
-                  <li><a href="agenda.html">Agenda</a></li>
-                  <li><a href="leituras-obrigatorias.html">Leituras obrigatórias</a></li>
+                    <li><a href="historia-do-vestibular.html">História do vestibular</a></li>
+                    <li><a href="principais-vestibulares.html">Principais vestibulares</a></li>
+                    <li><a href="agenda.html">Agenda</a></li>
+                    <li><a href="leituras-obrigatorias.html">Leituras obrigatórias</a></li>
                 </ul>
             </li>
-            <li><a href="matricula.html">Matrícula</a></li>            
+            <li><a href="matricula.html">Matrícula</a></li>
             <li><a href="galeria-de-fotos.html">Galeria de fotos</a></li>
             <li><a href="contato.html">Contato</a></li>
         </ul>
-        
+
         <div id="box-buscar">
             <form action="busca.html" method="get">
                 <input type="text" name="s" />
                 <input type="submit" value="" title="Buscar no Projeto Chance" />
             </form>
         </div>
-    </nav>
-    
-    
-</header>
+    </div>
+
+
+</div>
